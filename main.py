68d13ef575dab7255dc7034d271448f54ad07896
jpricardo <joaopds.ricardo@gmail.com>
2022-03-22 19:04:08 -0300

    Iniciando os trabalhos

--- main.py
+++ main.py
@@ -2,28 +2,54 @@
 from ofxparse import OfxParser
 import xlsxwriter
 import codecs
+import os
 
-with codecs.open('entrada.ofx') as fileobj:
-	ofx = OfxParser.parse(fileobj)
+def ler_arquivos(pasta):
 
-account = ofx.account
-statement = account.statement
-transactions = statement.transactions
-resultado = [[['DATA'], ['TIPO'], ['VALOR'], ['MEMO'],['NOME']]]
+	arquivos = []
 
-for transaction in transactions:
-	resultado.append([[transaction.date],[transaction.type],[transaction.amount],[transaction.memo],[transaction.payee]])
+	for filename in os.listdir(pasta):
+		f = os.path.join(pasta, filename)
+		if os.path.isfile(f):
+			with codecs.open(f) as fileobj:
+				ofx = OfxParser.parse(fileobj)
+				arquivos.append(ofx)
 
-planilha = xlsxwriter.Workbook('saida.xlsx')
+	return arquivos
 
-aba = planilha.add_worksheet()
 
-for linha in range(len(resultado)):
-	for coluna in range(len(resultado[linha])):
-		valor = resultado[linha][coluna][0]
-		aba.write(linha, coluna, valor)
+def gerar_planilha(obj):
 
-planilha.close()
+	account = obj.account
+	statement = account.statement
+	transactions = statement.transactions
+	resultado = [[['DATA'], ['TIPO'], ['VALOR'], ['MEMO'],['NOME']]]
 
+	for transaction in transactions:
+		resultado.append([[transaction.date],[transaction.type],[transaction.amount],[transaction.memo],[transaction.payee]])
+
+	return resultado
+
+
+def escrever_planilha(dados, pasta):
+
+	planilha = xlsxwriter.Workbook(f'{pasta}/saida.xlsx')
+	aba = planilha.add_worksheet()
+
+	for linha in range(len(resultado)):
+		for coluna in range(len(resultado[linha])):
+			valor = resultado[linha][coluna][0]
+			aba.write(linha, coluna, valor)
+
+	planilha.close()
+
+
+def main():
+	arquivos = ler_arquivos('entrada')
+	for arquivo in arquivos:
+		planilha = gerar_planilha(arquivo, 'saída')
+		escrever_planilha(planilha)
+
+main()
 #with open('saída.csv', 'a') as out:
     #out.write(resultado)
