121daafced60fce88c9cc8a713ffab3835cac5ee
ativasuporte <suporteativanautica@gmail.com>
2022-05-10 12:19:02 -0300

    Página de upload concluída, sistema de download funcionando 100%
    Co-Authored-By: João P. <jpricardo@users.noreply.github.com>

--- website/converter/views.py
+++ website/converter/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 
+from os import path
+
+import website.settings as settings
 from .models import Upload, Output
 from .forms import DocumentForm, handle_uploaded_file
 
@@ -16,26 +19,34 @@ def index(request):
 				new_doc = handle_uploaded_file(uploaded_file)
 				output_file = Output(document=new_doc, file_name=new_doc.name, original_file=file)
 				output_file.save()
-				return HttpResponseRedirect('resultado/1')
+				return HttpResponseRedirect(f'download/{output_file.id}')
 			except:
 				return a
 
 	else:
 		form = DocumentForm()
-	latest_uploads = Upload.objects.order_by('-uploaded_at')[:5]
+	latest_uploads = Upload.objects.order_by('-uploaded_at')[:10]
 	context = {
 		'latest_uploads': latest_uploads,
 		'form': form
 	}
 	return render(request, 'converter/index.html', context)
 
-def result(request, file_id):
+def output(request, file_id):
 	file = get_object_or_404(Upload, pk=file_id)
 	return render(request, 'converter/result.html', { 'file': file })
 
 
 def download(request, file_id):
-	return HttpResponse(f'Baixando arquivo {file_id}')
+	file = get_object_or_404(Output, pk=file_id)
+	relative_path = f'{file.document}'
+	file_path = path.join(settings.MEDIA_ROOT, relative_path)
+	if path.exists(file_path):
+		with open(file_path, 'rb') as fh:
+			response = HttpResponse(fh.read(), content_type='application/vnd.ms-excel')
+			response['Content-Disposition'] = 'inline; filename=' + path.basename(file_path)
+			return response
+	return render(request, 'converter/result.html', { 'file': file })
 
 def upload(request):
 	if request.method == 'POST':
