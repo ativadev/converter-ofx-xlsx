e97625ea7a0a79909f4d5f260472c6fff418549e
ativasuporte <suporteativanautica@gmail.com>
2022-05-10 12:19:22 -0300

    Alterações pra rodar o app em rede local
    Co-Authored-By: João P. <jpricardo@users.noreply.github.com>

--- website/website/settings.py
+++ website/website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-aogn_=dr16@m11690p%q9f9dwrr$1m=905gtizxm!cdvvkpw^=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "192.168.2.101"]
 
 
 # Application definition
--- website/website/wsgi.py
+++ website/website/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
+
+# FIX
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append(os.path.dirname(__file__))
 
 application = get_wsgi_application()
